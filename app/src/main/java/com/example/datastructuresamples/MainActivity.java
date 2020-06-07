9bd9aae6b5e6d99f919c6b7c32ee528fa8524440
Saket Shriwas <saket.shriwas@volvocars.com>
2020-06-07 10:27:07 +0200

    Implemented doubly linked lists

--- app/src/main/java/com/example/datastructuresamples/MainActivity.java
+++ app/src/main/java/com/example/datastructuresamples/MainActivity.java
@@ -9,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     MyDynamicArray<Car> array = new MyDynamicArray<>();
+    MyDoublyLinkedList<Car> myDoublyLinkedList = new MyDoublyLinkedList<>();
+
 
     //define local class Car
     class Car {
@@ -27,6 +29,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //workWithDynamicArrays();
+        dllOperations();
+    }
+
+
+    private void workWithDynamicArrays() {
         populateArray();
         //printArrayElements();
 
@@ -58,5 +66,66 @@ public class MainActivity extends AppCompatActivity {
             Car currentCar = array.getElementAt(i);
             Log.i("TAG", "Car Name = " + currentCar.name + ", Color = " + currentCar.color);
         }
+    }
+
+    //My doubly linked lists
+    private void dllOperations() {
+        populateMyDLL();
+        addNewElementToDLL();
+        removeFirstFromDll();
+        removeLastFromDll();
+        emptyDll();
+    }
+
+    //Add new items to the linked list
+    private void populateMyDLL() {
+        for (int i = 0; i < 10; i++) {
+            Car myCar = new Car("Car Name " + i, "Color " + i, "Manufacturer " + i);
+            //myDoublyLinkedList.addFirstElement(myCar);
+            myDoublyLinkedList.addLastElement(myCar);
+        }
+
+        Log.i("TAG", "DLL size after adding elements - " + myDoublyLinkedList.getLinked_list_size());
+    }
+
+    //Add element at specific index
+    public void addNewElementToDLL() {
+        Car myNewCar = new Car("Mercedes AMG", "Red", "Mercedes");
+        //Assuming we have 10 items in the dll, so adding this in between
+        myDoublyLinkedList.addElementAt(myNewCar, 5);
+
+        Car response = myDoublyLinkedList.getElementAtIndex(5);
+
+        Log.i("TAG", "Car added at index  5 - " + response.name);
+    }
+
+    //Remove first
+    public void removeFirstFromDll() {
+        //Print data before removing element
+        Log.i("TAG", "Original DLL size " + myDoublyLinkedList.getLinked_list_size());
+        Car firstCar = myDoublyLinkedList.getElementAtIndex(0);
+        Log.i("TAG", "First car name - " + firstCar.name);
+        myDoublyLinkedList.removeItemAtStart();
+        Log.i("TAG", "New DLL size " + myDoublyLinkedList.getLinked_list_size());
+        Car newFirstCar = myDoublyLinkedList.getElementAtIndex(0);
+        Log.i("TAG", "New First car name - " + newFirstCar.name);
+    }
+
+    //Remove last
+    public void removeLastFromDll() {
+        //Print data before removing element
+        Log.i("TAG", "Original DLL size " + myDoublyLinkedList.getLinked_list_size());
+        Car lastCar = myDoublyLinkedList.getElementAtIndex(myDoublyLinkedList.linked_list_size -1);
+        Log.i("TAG", "Last car name - " + lastCar.name);
+        myDoublyLinkedList.removeItemAtEnd();
+        Log.i("TAG", "New DLL size " + myDoublyLinkedList.getLinked_list_size());
+        Car newLastCar = myDoublyLinkedList.getElementAtIndex(myDoublyLinkedList.linked_list_size -1);
+        Log.i("TAG", "New Last car name - " + newLastCar.name);
+    }
+
+    //Clear all
+    public void emptyDll() {
+        myDoublyLinkedList.clearAll();
+        Log.i("TAG", "DLL size after empty - " + myDoublyLinkedList.getLinked_list_size());
     }
 }
---
+++ app/src/main/java/com/example/datastructuresamples/MyDoublyLinkedList.java
@@ -0,0 +1,207 @@
+package com.example.datastructuresamples;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+/**
+ * Linked list is a set of nodes which contain data and each node holds reference to another node holding data.
+ * All LLs have a head node and a tail node.
+ * There are 2 types- Singly Linked Lists and Doubly Linked lists.
+ * Singly Linked lists - each node holds data and reference to next node. Hence traversing is possible
+ * only in one direction in the list.
+ * Doubly Linked lists - each node holds data and reference to previous and next nodes. Hence traversing
+ * is possible in both directions.
+ * @param <T>
+ */
+public class MyDoublyLinkedList<T> implements Iterable<T> {
+
+    //class representing node in the linked list
+    private class Node<T> {
+        private T data;
+        private Node<T> previous;
+        private Node<T> next;
+
+        Node(T element, Node<T> prev, Node<T> next){
+            this.data = element;
+            this.previous = prev;
+            this.next = next;
+        };
+    }
+
+    Node<T> head = null;    //By default list is empty
+    Node<T> tail = null;
+    int linked_list_size = 0;
+
+
+    //Add node to the start of the list, O(1)
+    public void addFirstElement(T element) {
+        //if LL is empty then simply create a new node and have it assigned as head and tail and set size to 1
+        if (linked_list_size == 0) {
+            head = tail = new Node<>(element, null, null);
+        } else {
+            //new node which holds data element and whose next pointer points to current head
+            Node<T> newNode = new Node<>(element, null, head);
+            //update current head's previous to refer to the new node
+            head.previous = newNode;
+            //head = newNode makes head point to newNode. But
+            //head's original node is not affected....it still has its original value.
+            //So basically head is a pointer to the object.
+            //When we assign head to newNode it does not update its values to those of NewNode.
+            //Instead it starts pointing to newNode object.
+            //The original head node is referenced via newNode's next pointer.
+            head = newNode;
+        }
+
+        linked_list_size++;
+    }
+
+    //Add node to end of list
+    public void addLastElement(T element) {
+        //if LL is empty then simply create a new node and have it assigned as head and tail and set size to 1
+        if (linked_list_size == 0) {
+            head = tail = new Node<>(element, null, null);
+        } else {
+            //add new node which holds element data and whose previous pointer refers the current tail node
+            Node<T> newNode = new Node<>(element, tail, null);
+            //current tail's next pointer refers the new node
+            tail.next = newNode;
+            //update head to new Node
+            tail = newNode;
+        }
+        linked_list_size++;
+    }
+
+    //Add node at specific index
+    public void addElementAt(T element, int index) {
+        //Check if index is within size.
+        if (index < linked_list_size) {
+            //Set pointer traverse to head
+            Node<T> traverse = head;
+            //Traverse to the node which points to the index where we wish to add the new element
+            for (int i = 0; i < linked_list_size; i++) {
+                if (i != index - 1) {
+                    traverse = traverse.next;
+                } else {
+                    //Exit loop
+                    break;
+                }
+            }
+            //Now we create a new node with element data and whose previous pointer points to
+            // current node referred by traverse and whose next node points to next referred by traverse referred node..
+            Node<T> newNode = new Node<>(element, traverse, traverse.next);
+            //Now existing traverse node next points to new Node
+            traverse.next = newNode;
+            //increment size by 1
+            linked_list_size++;
+        } else {
+            //Else throw exception
+            throw new IllegalArgumentException("Index is greater than size of linked list");
+        }
+    }
+
+    //Remove node from start of the list
+    public void removeItemAtStart() {
+        if (linked_list_size == 0) {
+            Log.i("TAG", "List is empty");
+            return;
+        }
+        //Assign pointer to the original head
+        Node<T> old_head = head;
+        //move head to next pointer
+        head = head.next;
+        //Delete current head
+        old_head = null;
+        //reduce size by 1
+        linked_list_size--;
+    }
+
+    //Remove node from end of the list
+    public void removeItemAtEnd() {
+        if (linked_list_size == 0) {
+            Log.i("TAG", "List is empty");
+            return;
+        }
+        //Assign pointer to original tail
+        Node<T> original_tail = tail;
+        //move tail to previous pointer
+        tail = tail.previous;
+        //Delete current tail
+        original_tail = null;
+        linked_list_size--;
+    }
+
+    //Find index of node in the list
+    public int getIndexOf(T element) {
+        //Get pointer to head
+        Node<T> current_node = head;
+        for (int i = 0; i < linked_list_size; i++) {
+            if (current_node.data == element) {
+              return i;
+            }
+        }
+        return -1;  //Index not found
+    }
+
+    //Find node at index
+    public T getElementAtIndex(int index) {
+        //Get pointer to head
+        Node<T> current_node = head;
+        for (int i = 0; i < linked_list_size; i++) {
+            if (i == index) {
+                return current_node.data;
+            }else {
+                current_node = current_node.next;
+            }
+        }
+        return null;    //Index not found
+    }
+
+    //Get size of the list
+    public int getLinked_list_size() {
+        return linked_list_size;
+    }
+
+    //Empty the list
+    public void clearAll() {
+        while (head.next != null) {
+            Node<T> current_node = head;
+            //Move head to next node
+            head = head.next;
+            //Delete current node
+            current_node = null;
+            linked_list_size--;
+        }
+    }
+
+    @NonNull
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public T next() {
+                return null;
+            }
+        };
+    }
+
+    @Override
+    public void forEach(@NonNull Consumer<? super T> action) {
+
+    }
+
+    @NonNull
+    @Override
+    public Spliterator<T> spliterator() {
+        return null;
+    }
+}
